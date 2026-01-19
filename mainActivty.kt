62a5e06e809ffa4cb84b408a6a8a1713e920c1e3
rajesh160701 <rajesrahmanputra160701@gmail.com>
2026-01-20 02:05:19 +0700

    Create mainActivty.kt

---
+++ mainActivty.kt
@@ -0,0 +1,40 @@
+package com.example.webviewutmj
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+
+        // Setting WebView
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+
+        // Load website
+        webView.loadUrl("https://ecampus.utmj.ac.id")
+    }
+
+    // Agar tombol back kembali ke halaman sebelumnya
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
